f78319ae67de26e21439dcb6448e774259645553
tw Shim <marocchino@tws-vm.(none)>
2010-03-26 10:10:50 +0900

    뷰와 헬퍼의 분리

--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -7,11 +7,11 @@ class NoticesLinkRenderer < WillPaginate::LinkRenderer
   def to_html
     links = @options[:page_links] ? windowed_links : []
     # previous/next buttons
-    links.unshift page_link_or_span(@collection.previous_page, 'disabled prev_page', @options[:previous_label])
-    links.unshift page_link_or_span(1, '', "First")
-    links.push    page_link_or_span(@collection.next_page,     'disabled next_page', @options[:next_label])
-    links.push    page_link_or_span(@collection.total_pages, '', "Last")
-    links.push    page_span(@collection.total_pages, "total #{@collection.total_pages} pages")
+    links.unshift page_link_or_span(@collection.previous_page, '', @options[:previous_label])
+    links.unshift page_link_or_span(1, '', @options[:last_label])
+    links.push    page_link_or_span(@collection.next_page,     '', @options[:next_label])
+    links.push    page_link_or_span(@collection.total_pages, '', @options[:first_label])
+    links.push    page_span(@collection.total_pages, @options[:total_label].sub("##",@collection.total_pages.to_s))
 
     html = links.join(@options[:separator])
     @options[:container] ? @template.content_tag(:div, html, html_attributes) : html
